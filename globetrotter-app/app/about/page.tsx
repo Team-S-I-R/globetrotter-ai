0cd237506d83208659fb1166544dd515b5c08fb4
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-08-18 06:31:27 -0400

    more backticks

--- globetrotter-app/app/about/page.tsx
+++ globetrotter-app/app/about/page.tsx
@@ -15,7 +15,7 @@ export default function About() {
         className="select-none w-full p-4 text-center h-screen gap-4 flex flex-col place-items-center place-content-center">
             <h1 className="text-3xl font-bold">About Us</h1>
             <p>We are a passionate team dedicated to creating the best software solutions for all people, from students to professionals. This project started development in mid-August 2024.</p>
-            <p>We are thrilled to share that we've entered this project into a hackathon with Headstarter! Wish us luck :D</p>
+            <p>We are thrilled to share that we have entered this project into a hackathon with Headstarter! Wish us luck :D</p>
             <div className="flex flex-col place-content-center place-items-center w-md gap-4">
                 <div className="w-max flex gap-4">
                     <h2>Shaurya Bisht</h2>
